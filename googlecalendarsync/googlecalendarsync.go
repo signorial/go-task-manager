e632ed6718c49e3c60f345020cc4337a8689be1f
luke fraser <lufraser@gmail.com>
2026-06-17 12:50:49 -0600

    started on the google calender task syncing

---
+++ googlecalendarsync/googlecalendarsync.go
@@ -0,0 +1,96 @@
+package googlecalendarsync
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+type LocalTask struct {
+	ID            string    `json:"id"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	StartTime     time.Time `json:"start_time"`
+	EndTime       time.Time `json:"end_time"`
+	GoogleEventID string    `json:"google_event_id"`
+	UpdateAt      time.Time `json:"update_at"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	// read the google api credentials
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		fmt.Errorf("unable to read the credentials file: %v", err)
+	}
+
+	// reques read write access to calendar
+	config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+	if err != nil {
+		fmt.Errorf("Unable to parse client secret file: %v", err)
+	}
+
+	client := getClient(config)
+
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		fmt.Errorf("Unable to retrieve Calendar client: %v", err)
+	}
+}
+
+// --- OAUTH2 UTILITIES FOR TOKEN MANAGEMENT ---
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		fmt.Errorf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.Background(), authCode)
+	if err != nil {
+		fmt.Errorf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, nil
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		fmt.Errorf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
