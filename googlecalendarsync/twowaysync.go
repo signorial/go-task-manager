72f9274ee5a542285a91a55632546fc5d352510a
signorial <lufraser@gmail.com>
2026-07-19 20:45:45 -0600

    no change

--- googlecalendarsync/twowaysync.go
+++ googlecalendarsync/twowaysync.go
@@ -334,6 +334,7 @@ func pushLocalChanges(db *sqlx.DB, srv *calendar.Service) error {
 			End:         &calendar.EventDateTime{DateTime: ev.EndTime},
 		}
 		// TODO: fix the start tiem end time logic to handle dates and datetimes this is a change
+		// TODO need to return laptop
 
 		var apiErr error
 		if isLocalOnlyID(ev.ID) {
