40624d1bef10a5a49fd23ec709a3da5ea0c116a8
signorial <lufraser@gmail.com>
2026-07-29 11:29:24 -0600

    no change

--- db/queries/createtables.sql
+++ db/queries/createtables.sql
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS events (
     deleted INTEGER DEFAULT 0,
     task_id INTEGER REFERENCES tasks (task_id)
 );
+select * from tasks
+
 
 --TODO: need to figure out how to run queries directly in neovim
---TODO: no reason
